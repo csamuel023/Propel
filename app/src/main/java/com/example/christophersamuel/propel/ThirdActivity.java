9d93511a07af68ff879bb45b46bbf50395255aa1
phatminhdang <dangminhphat1@gmail.com>
2018-04-19 15:17:28 -0400

    Initial commit

--- app/src/main/java/com/example/christophersamuel/propel/ThirdActivity.java
+++ app/src/main/java/com/example/christophersamuel/propel/ThirdActivity.java
@@ -1,13 +1,29 @@
 package com.example.christophersamuel.propel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
-
+private Button bGoals;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        bGoals = (Button) findViewById(R.id.bGoals);
+        bGoals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGoal();
+            }
+        });
+
+    }
+    public void openGoal(){
+        Intent intent = new Intent(this,Goals.class);
+        startActivity(intent);
     }
 }
