6356d88ba039b78ff05755b66817a28ce81ac274
jcrawf20 <36489074+jcrawf20@users.noreply.github.com>
2018-04-21 03:30:35 -0400

    Add files via upload
    BodyInfo saves data to text file "savedBodyInfo"
    MoveNotification does notification stuff
    JakeMainActivity has a bunch of miscellanious files that I haven't had time to make work outside of MainActivity

---
+++ app/src/main/java/com/example/christophersamuel/propel/BodyInfo.java
@@ -0,0 +1,53 @@
+package com.example.jake.workoutapp;
+
+import android.content.Context;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BodyInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bodyinfo);
+    }
+
+
+    public static void saveBodyInfo(String height, String weight, String age, boolean gender, boolean active, Context context) throws IOException //true = male, active
+    {
+        String filename = "savedBodyInfo";
+        FileOutputStream out = null;
+        String content = "Height: " + height + "\nWeight: " + weight + "\nAge: " + age + "\nGender: ";
+        content += (gender) ? "male" : "female";
+
+        try//Tries opening file as if it already exists
+        {
+            out = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            out.write(content.getBytes());
+        }
+        catch(FileNotFoundException e)//Creates new file if it doesn't exist
+        {
+            out = new FileOutputStream(filename);
+            out.write(content.getBytes());
+        }
+        finally
+        {
+            out.close();
+        }
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/christophersamuel/propel/MoveNotification.java
@@ -0,0 +1,27 @@
+package com.example.jake.workoutapp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Jake on 4/21/2018.
+ */
+
+public class MoveNotification extends BroadcastReceiver
+{
+    public static final String NOTIFICATION_ID = "MOVE_NOTIFICATION";
+    public static final String NOTIFICATION = "Get up and move!";
+
+    @Override
+    public void onReceive(final Context context, Intent intent)
+    {
+        NotificationManager notificationManager = (NotificationManager) (context.getSystemService(Context.NOTIFICATION_SERVICE));
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int notificationId = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(notificationId, notification);
+    }
+}
