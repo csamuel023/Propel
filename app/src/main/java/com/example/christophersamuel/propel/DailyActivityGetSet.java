a2eb0f9d9676df8d0e4fccca740bbe4d6b6329ca
phatminhdang <dangminhphat1@gmail.com>
2018-04-22 17:35:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/christophersamuel/propel/DailyActivityGetSet.java
@@ -0,0 +1,60 @@
+package com.example.christophersamuel.propel;
+
+public class DailyActivityGetSet {
+    String legs, chest, biceps, back, shoulders, core, cardio;
+
+    public void setLegs(String legs) {
+        this.legs = legs;
+    }
+
+    public String getLegs() {
+        return this.legs;
+    }
+
+    public void setChest(String chest) {
+        this.chest = chest;
+    }
+
+    public String getChest() {
+        return this.chest;
+    }
+    public void setBiceps(String biceps) {
+        this.biceps = biceps;
+    }
+
+    public String getBiceps() {
+        return this.biceps;
+    }
+
+    public void setBack(String back) {
+        this.chest = back;
+    }
+
+    public String getBack() {
+        return this.back;
+    }
+    public void setShoulders(String shoulders) {
+        this.shoulders = shoulders;
+    }
+
+    public String getShoulders() {
+        return this.shoulders;
+    }
+
+    public void setCore(String core) {
+        this.core = core;
+    }
+
+    public String getCore() {
+        return this.core;
+    }
+    public void setCardio(String cardio) {
+        this.cardio = cardio;
+    }
+
+    public String getCardio() {
+        return this.cardio;
+    }
+
+
+}
